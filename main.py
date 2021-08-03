3a95670ae7bee6b2a0282e44bd79391d9fb1ec8e
Joe <abc@gmail.com>
2021-08-03 14:55:56 -0400

    changed main to add error handling

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from functions import add
 from functions import subtract
 from functions import multiply
 from functions import divide
+from error_handling import handle_division
+from error_handling import handle_addition
+from error_handling import handle_multiplication
+from error_handling import handle_subtraction
 
 def main():
     print (add(2,3))
     print (subtract(3,2))
     print(multiply(2,3))
-    print(divide(4,2))
+    print(divide(4,3))
 
 if __name__ == "__main__":
     # execute only if run as a script
