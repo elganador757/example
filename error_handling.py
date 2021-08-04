78dd47f00394259e3b7043c8ac1fdba57ac8ea29
Joe <abc@gmail.com>
2021-08-04 11:38:32 -0400

    Updtaed error handling for loop

--- error_handling.py
+++ error_handling.py
@@ -1,16 +1,12 @@
 
 def menu_selection_error(a): 
     """if input is a non-empty string, throw an error"""
-    if a < 1 or a > 5:
-             print("'\033[91m'ERROR:'\033[92m'Invalid input! Please select from the menu")
+    if a < '1' or a > '5':
+             print("'\033[91m'ERROR:'\033[92m'Invalid input! Please select from the menu'\033[0m'")
 
 
 def handle_division(a):
     """makes sure number is not zero"""
     if a == 0:
-        print("'\033[91m'ERROR:'\033[92m'Cannot divide by zero!")
+        print("'\033[91m'ERROR:'\033[92m'Cannot divide by zero!'\033[0m'")
     
-menu_selection_error(45)
-menu_selection_error(4)
-
-handle_division(0)
